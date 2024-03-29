79b493625e799b48f1792d5bcd8085504ec66206
dearparthsingh <dearparthsingh@gmail.com>
2024-03-29 09:51:01 +0000

    fewf

--- src/views/signin.vue
+++ src/views/signin.vue
@@ -30,30 +30,33 @@
             </div>
             <div style="margin-top: 20px;">
               <!-- <button class="form-button" type="button" style="background-color: white; color: black; margin-bottom: 10px; margin-top: 10px; align-content:;">Get started</button> -->
-              <button @click="redirectToMain" class="form-button-google" type="button" style="background-color: #1a1a1a; color: white; border-radius: 10px; margin-bottom: 10px; margin-top: 10px;">Continue</button>
+              <button @click="redirectToMain" class="form-button-google" type="button" style="background-color: #1a1a1a; color: white; border-radius: 10px; margin-bottom: 10px; margin-top: 10px; ">Continue</button>
 
             </div>
           </div>
         </form>
         <div class="sign-google">
-          <button class="form-button-google" type="button" style="background-color: white; color: black;border-radius: 10px; ">
+          <button class="form-button-google" type="button" style="background-color: white; color: black;border-radius: 10px; ">Continue with Google
 
-            <span class="mr-2 inline-block" style="align-items: center;">
-              <svg
-              fill="currentColor"
-              viewBox="0 0 25  25" 
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 text-rose-500"
-              width="15px"
-              height="10px"
-              
-            >
-              <path
-                d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"
-              ></path>
-            </svg>
+            <span class="mr-2 inline-block" style="align-items: center;"> 
+              <div class="ae ak be"><svg width="18" height="18" viewBox="0 0 256 262"
+                                                        xmlns="http://www.w3.org/2000/svg" class="cw cx"
+                                                        preserveAspectRatio="xMidYMid">
+                                                        <path
+                                                            d="M255.9 133.5c0-10.8-.9-18.6-2.8-26.7H130.6v48.4h71.9a63.8 63.8 0 01-26.7 42.4l-.2 1.6 38.7 30 2.7.3c24.7-22.8 38.9-56.3 38.9-96"
+                                                            fill="#4285F4"></path>
+                                                        <path
+                                                            d="M130.6 261.1c35.2 0 64.8-11.6 86.4-31.6l-41.2-32c-11 7.8-25.8 13.1-45.2 13.1a78.6 78.6 0 01-74.3-54.2l-1.5.1-40.3 31.2-.6 1.5C35.4 231.8 79.5 261 130.6 261"
+                                                            fill="#34A853"></path>
+                                                        <path
+                                                            d="M56.3 156.4a80.4 80.4 0 01-.2-51.7V103L15.3 71.3l-1.4.6a130.7 130.7 0 000 117.3l42.4-32.8"
+                                                            fill="#FBBC05"></path>
+                                                        <path
+                                                            d="M130.6 50.5c24.5 0 41 10.6 50.4 19.4L218 34C195.2 13 165.8 0 130.6 0 79.5 0 35.4 29.3 13.9 72l42.2 32.7a79 79 0 0174.4-54.2"
+                                                            fill="#EB4335"></path>
+                                                    </svg></div>
            
-            Sign in with Google 
+            <!-- Sign in with Google  -->
 
             </span>
             <!-- <img src="./favicon.ico"> -->
@@ -61,31 +64,27 @@
           
         </div>
 
-        <div class="sign-google">
-          <button class="form-button-google" type="button" style="background-color: white; color: black;border-radius: 10px; margin-top: 10px; ">
+        <div class="sign-apple" style="margin-top:10px">
+          <button class="form-button-google" type="button" style="background-color: white; color: black;border-radius: 10px; ">Continue with Apple
 
-            <span class="mr-2 inline-block" style="align-items: center;">
-              <!-- <svg
-              fill="currentColor"
-              viewBox="0 0 25  25" 
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 text-rose-500"
-              width="15px"
-              height="10px"
-              
-            >
-              <path
-                d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"
-              ></path>
-            </svg> -->
-  
-
-            🍎 Sign in with Apple 
+            <span class="mr-2 inline-block" style="align-items: center;"> 
+              <div class="ae ak be"><svg width="22" height="22" viewBox="0 0 22 22"
+                                                        fill="#000000" xmlns="http://www.w3.org/2000/svg" class="cw cx">
+                                                        <g transform="matrix( 1 0 0 1 3 1 )">
+                                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                                                d="M11.2676 3.23104C11.9166 2.39314 12.4087 1.20886 12.2307 0C11.1698 0.0737252 9.92984 0.752465 9.2063 1.63717C8.54675 2.43878 8.00468 3.63126 8.21639 4.78863C9.37613 4.8249 10.5731 4.12978 11.2676 3.23104ZM17 14.6022C16.5359 15.6367 16.3125 16.099 15.7146 17.0153C14.8806 18.2943 13.7046 19.887 12.2459 19.8987C10.9512 19.9128 10.6173 19.0503 8.85967 19.0608C7.10203 19.0702 6.73561 19.9151 5.4386 19.9022C3.98106 19.8894 2.86668 18.4523 2.03264 17.1732C-0.300809 13.5993 -0.546251 9.404 0.892672 7.17235C1.91632 5.58785 3.53089 4.66101 5.04775 4.66101C6.59136 4.66101 7.56267 5.51295 8.84106 5.51295C10.0811 5.51295 10.836 4.65867 12.6216 4.65867C13.9733 4.65867 15.4052 5.39944 16.4242 6.67734C13.0834 8.5193 13.6243 13.3185 17 14.6022Z"
+                                                                fill="#000000" opacity="1"></path>
+                                                        </g>
+                                                    </svg></div>
+           
+            <!-- Sign in with Google  -->
 
             </span>
             <!-- <img src="./favicon.ico"> -->
           </button>
+          
         </div>
+
 
 
 
@@ -168,11 +167,24 @@ span{
     justify-content: center;
     align-items: auto;
     text-align: auto;
-    font-family: "Uber Move",
+    font-family: "Uber Move";
+  }
+  .sign-apple{
+    display: flex;
+    justify-content: center;
+    align-items: auto;
+    text-align: auto;
+    font-family: "Uber Move";
   }
 
 .form-button-google:hover {
   background-color: var(--hover-color); /* Using hover color variable */
   border-color: var(--border-color); /* Using border color variable */
+}
+
+.logo-svg{
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 5px; /* Adjust margin as needed */
 }
 </style>
