febb416c486ce965a0ad7c161fa7329abd9f3bc5
prth1234 <107931755+prth1234@users.noreply.github.com>
2024-04-09 18:29:38 +0530

    done with footer

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -3,14 +3,14 @@
       <!-- Main content here -->
       <hr class="divider">
       <footer class="uber-footer">
-      
         <div class="footer-container">
+          <div class="footer-logo">
             <a href="#" class="navbar-brand" style="font-size: 30px;">
               <span style="background-image: linear-gradient(135deg, #8a4ba8, #ff66c4, #282c3d); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">Uber</span>
               <span style="color: #06C167;"><b> Eats</b></span>
             </a>
-           
-          <div class="footer-logo" style="margin-bottom: 40px;">
+          </div>
+          <div class="footer-logo">
             
           </div>
           <div class="links-container">
@@ -38,13 +38,26 @@
             </div>
           </div>
         </div>
-        <div class="social-icons" style="margin-top: 40px;">
-          <a href="#"><img src="https://www.example.com/facebook-icon.png" alt="Facebook"></a>
-          <a href="#"><img src="https://www.example.com/twitter-icon.png" alt="Twitter"></a>
-          <a href="#"><img src="https://www.example.com/instagram-icon.png" alt="Instagram"></a>
+        <br/> <br/>
+        <div class="social-icons">
+          <a href="#"><svg aria-hidden="true" focusable="false" viewBox="0 0 16 16" class="hp du es et" width="16" height="16"><path d="M14.667 0H1.333C.597 0 0 .597 0 1.333v13.334C0 15.403.597 16 1.333 16h7.334v-6h-2V7.333h2V5.61c0-2.067 1.262-3.192 3.106-3.192.883 0 1.642.066 1.863.095v2.16h-1.279c-1.002 0-1.196.477-1.196 1.176v1.485h2.96L13.454 10h-2.293v6h3.506c.736 0 1.333-.597 1.333-1.333V1.333C16 .597 15.403 0 14.667 0z"></path></svg></a>
+          <a href="#"><svg aria-hidden="true" focusable="false" viewBox="0 0 14 12" class="hp du es et" width="16" height="16"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.973 1.429a5.556 5.556 0 01-1.649.476A3.02 3.02 0 0013.587.231c-.554.34-1.169.59-1.823.728A2.792 2.792 0 009.668 0c-1.585 0-2.87 1.356-2.87 3.025 0 .24.026.47.074.692-2.385-.12-4.5-1.328-5.916-3.154-.249.443-.388.96-.388 1.522 0 1.051.507 1.976 1.276 2.52a2.754 2.754 0 01-1.3-.378v.036c0 1.467.988 2.692 2.302 2.97a2.776 2.776 0 01-1.29.052c.368 1.202 1.426 2.077 2.685 2.102A5.566 5.566 0 010 10.641 7.856 7.856 0 004.407 12c5.282 0 8.168-4.611 8.168-8.604 0-.13 0-.259-.01-.388.56-.424 1.05-.959 1.435-1.567l-.027-.012z"></path></svg></a>
+          <a href="#"><svg aria-hidden="true" focusable="false" viewBox="0 0 16 16" class="hp du es et" width="16" height="16"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C5.827 0 5.555.01 4.702.048 3.85.088 3.27.222 2.76.42a3.908 3.908 0 00-1.417.923c-.445.444-.72.89-.923 1.417-.198.51-.333 1.09-.372 1.942C.008 5.555 0 5.827 0 8s.01 2.445.048 3.298c.04.852.174 1.433.372 1.942.204.526.478.973.923 1.417.444.445.89.72 1.417.923.51.198 1.09.333 1.942.372.853.04 1.125.048 3.298.048s2.445-.01 3.298-.048c.852-.04 1.433-.174 1.942-.372a3.908 3.908 0 001.417-.923c.445-.444.72-.89.923-1.417.198-.51.333-1.09.372-1.942.04-.853.048-1.125.048-3.298s-.01-2.445-.048-3.298c-.04-.852-.174-1.433-.372-1.942a3.908 3.908 0 00-.923-1.417A3.886 3.886 0 0013.24.42c-.51-.198-1.09-.333-1.942-.372C10.445.008 10.173 0 8 0zm0 1.44c2.136 0 2.39.01 3.233.048.78.036 1.203.166 1.485.276.374.145.64.318.92.598.28.28.453.546.598.92.11.282.24.705.276 1.485.038.844.047 1.097.047 3.233s-.01 2.39-.05 3.233c-.04.78-.17 1.203-.28 1.485-.15.374-.32.64-.6.92-.28.28-.55.453-.92.598-.28.11-.71.24-1.49.276-.85.038-1.1.047-3.24.047s-2.39-.01-3.24-.05c-.78-.04-1.21-.17-1.49-.28a2.49 2.49 0 01-.92-.6c-.28-.28-.46-.55-.6-.92-.11-.28-.24-.71-.28-1.49-.03-.84-.04-1.1-.04-3.23s.01-2.39.04-3.24c.04-.78.17-1.21.28-1.49.14-.38.32-.64.6-.92.28-.28.54-.46.92-.6.28-.11.7-.24 1.48-.28.85-.03 1.1-.04 3.24-.04L8 1.44zm0 2.452a4.108 4.108 0 100 8.215 4.108 4.108 0 000-8.215zm0 6.775a2.667 2.667 0 110-5.334 2.667 2.667 0 010 5.334zm5.23-6.937a.96.96 0 11-1.92 0 .96.96 0 011.92 0z"></path></svg></a>
         </div>
-        <div class="legal-text">
-          <p>&copy; 2024 Uber Technologies Inc. | <a href="#">Privacy Policy</a> | <a href="#">Terms</a> | <a href="#">Pricing</a> | <a href="#">Do not sell or share my personal information</a></p>
+        <br/>
+
+
+        <div class="display-download-on">
+        <a href="#">
+            <img alt="Download on the App Store" role="img" src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/783bb4a82e5be29e.svg" loading="lazy" decoding="async" height="40" width="135" class="dt">        </a>
+        <a href="#">
+            <img alt="Get it on Google Play" src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/163bdc9b0f1e7c9e.png" loading="lazy" decoding="async" height="40" width="134" class="dt">        </a>
+       
+        </div>
+        <hr class="divider">
+
+            <div class="legal-text">
+          <p>&copy; 2024 Uber Technologies Inc.<a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy</a><a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Terms</a><a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Pricing</a><a href="#">Do not sell or share my personal information</a></p>
           <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
         </div>
       </footer>
@@ -69,6 +82,14 @@
     height: 1px;
     background-color: #1d1b1b;
   }
+
+  .display-download-on{
+    display: flex;
+    justify-content:center;
+    gap:10px;
+    margin-bottom: 40px;
+
+  }
   
   .footer-container {
     display: flex;
@@ -78,18 +99,16 @@
   
   .footer-logo img {
     height: 50px;
-
   }
   
   .links-container {
     display: flex;
-    gap:80px;
-    width:600px;
+    gap: 80px;
+    width: 600px;
     max-width: 1200px;
-    font-family: 'Uber Light';
-color: #555050;
-text-align: left;
-
+    font-family: "Uber Move Light";
+    color: #06c167;
+    text-align: left;
   }
   
   .footer-links {
@@ -97,12 +116,6 @@ text-align: left;
     margin-top: 70px;
     font-size: 17px;
   }
-
-
-
-
-
-
   
   .footer-links h3 {
     color: #fff;
@@ -120,21 +133,32 @@ text-align: left;
   .footer-links a {
     color: #fff;
     text-decoration: none;
+    margin-bottom: 5px; /* Added margin-bottom to create a gap */
   }
   
   .social-icons a {
     margin-right: 10px;
   }
   
-  .social-icons img {
-    height: 30px;
+  .social-icons svg {
+    width: 16px;
+    height: 16px;
+    fill: #fff;
   }
   
   .legal-text {
-    margin-top: 20px;
+    margin-top: 40px;
     text-align: center;
   }
-
- 
+  
+  @font-face {
+    font-family: "Uber Move Light";
+    src: url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.eot");
+    src: url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.eot?#iefix") format("embedded-opentype"),
+         url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.woff2") format("woff2"),
+         url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.woff") format("woff"),
+         url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.ttf") format("truetype"),
+         url("https://db.onlinewebfonts.com/t/79dd8fd82129493e2894679950d5b897.svg#Uber Move Light") format("svg");
+  }
   </style>
   
