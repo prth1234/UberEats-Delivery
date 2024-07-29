05ce1ecc9b2aed24fbc164b19261746a7e71c8ef
prth1234 <107931755+prth1234@users.noreply.github.com>
2024-07-29 22:49:05 +0530

    more changes

--- src/views/DetailView/Desserts.vue
+++ src/views/DetailView/Desserts.vue
@@ -2,165 +2,166 @@
     <div class="picked-for-you">
       <div class="grid-container">
         <div class="grid-item" v-for="(item, index) in items" :key="index">
-          <div class="item-image-container">
-            <img :src="item.image" :alt="item.name" class="item-image" />
-          </div>
-          <div class="item-info">
-            <h3>{{ item.name }}</h3>
-            <p class="price">${{ item.price.toFixed(2) }}</p>
-            <div class="quantity-control">
-              <button class="cart-button" @click="addToCart(index)">
-                <span v-if="item.quantity > 0">+</span>
-                <span v-else>+</span>
-              </button>
-              <div v-if="item.quantity == 0">0</div>
-              <div v-if="item.quantity > 0">{{ item.quantity }}</div>
-              <button class="cart-button" @click="removeFromCart(index)">
-                <span>-</span>
-              </button>
+          <div class="item-content">
+            <div class="image-container">
+              <img :src="item.image" :alt="item.name" class="item-image" />
+            </div>
+            <div class="item-info">
+              <h3>{{ item.name }}</h3>
+              <p class="price">${{ item.price.toFixed(2) }}</p>
+              <div class="quantity-control">
+                <button class="cart-button" @click="addToCart(index)">+</button>
+                <span>{{ item.quantity }}</span>
+                <button class="cart-button" @click="removeFromCart(index)">-</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </template>
-
-
-
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          name: "Chick-fil-A® Hot&Spicy",
-          price: 10.29,
-          image:
-            "https://i.ibb.co/MgJVR9m/CFA51250-Summer24-Maple-Pepper-Bacon-On-White-Web-PLP-Hero-D-710x580-jpg-master-removebg-preview.png",
-          rank: "＃ 1 most ordered",
-          quantity: 0,
-        },
-        {
-          name: "Chick-fil-A® Bacon",
-          price: 8.55,
-          image:
-            "https://pngimg.com/d/fries_PNG27.png",
-          rank: "＃ 2 most ordered",
-          quantity: 0,
-        },
-        {
-          name: "Spicy Southwest Pickle",
-          price: 14.55,
-          image:
-            "https://static.vecteezy.com/system/resources/previews/039/113/424/non_2x/ai-generated-color-full-veggie-wrap-isolated-on-background-free-png.png",
-          rank: "＃ 3 most ordered",
-          quantity: 0,
-        },
-        // {
-        //   name: "Chick-fil-A ChickMeal® Meal",
-        //   price: 13.09,
-        //   image:
-        //     "https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/PLP%20updates/New%20menu%20module%20images/Spicy%20Chicken%20Sandwich.png?h=-1&w=-1&la=en",
-        //   quantity: 0,
-        // },
-      ],
-      currentIndex: 0,
-    };
-  },
-  methods: {
-    addToCart(index) {
-      this.items[index].quantity++;
+  
+  <script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            name: "Chick-fil-A® OG Waffles",
+            price: 10.29,
+            image: "https://b.zmtcdn.com/data/dish_photos/ff6/89221ba732134c354e5e36fc784aeff6.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+          {
+            name: "Chick-fil-A® Fruit Cup",
+            price: 8.55,
+            image: "https://b.zmtcdn.com/data/dish_photos/ab9/008928907d91882101e09f998d923ab9.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+          {
+            name: "Side Salad",
+            price: 14.55,
+            image: "https://b.zmtcdn.com/data/dish_photos/9a6/5687a4c934c9489c53342f80388fd9a6.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+          {
+            name: "ChickMeal® Waffles Chips",
+            price: 13.09,
+            image: "https://b.zmtcdn.com/data/dish_photos/b51/a7d5bb66b985629fcdbce8c1cb82bb51.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+          {
+            name: "ChickSalad® Noodle Soup",
+            price: 13.09,
+            image: "https://b.zmtcdn.com/data/dish_photos/5fc/32e9fd54fb72f021de4ff143fbf825fc.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+          {
+            name: "Chick-n-Macros Kale",
+            price: 13.09,
+            image: "https://b.zmtcdn.com/data/dish_photos/045/89a10bed2a9df5f1cc17b71cf03af045.jpeg?fit=around|130:130&crop=130:130;*,*",
+            quantity: 0,
+          },
+        ],
+      };
     },
-    removeFromCart(index) {
-      if (this.items[index].quantity > 0) {
-        this.items[index].quantity--;
-      }
+    methods: {
+      addToCart(index) {
+        this.items[index].quantity++;
+      },
+      removeFromCart(index) {
+        if (this.items[index].quantity > 0) {
+          this.items[index].quantity--;
+        }
+      },
     },
-  },
-};
-</script>
-<style scoped>
-.picked-for-you {
-  font-family: Arial, sans-serif;
-  margin-bottom: 70px;
-}
-
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 1rem;
-}
-
-.grid-item {
-  border: 1px solid #202020;
-  border-radius: 20px;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-}
-
-.item-image-container {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem;
-  min-height: 200px; /* Set a minimum height for the image container */
-}
-
-.item-image {
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: contain;
-}
-
-.item-info {
-  padding: 1rem;
-  background-color: transparent;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.item-info h3 {
-  font-size: 1rem;
-  margin: 0;
-  text-align: center;
-}
-
-.item-info .price {
-  font-size: 0.875rem;
-  color: #888;
-  margin: 0.5rem 0;
-}
-
-.quantity-control {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.cart-button {
-  background-color: transparent;
-  padding: 0.5rem;
-  border-radius: 50%;
-  cursor: pointer;
-  margin: 0 0.5rem;
-  color: green;
-  border: 1px solid transparent;
-  width: 35px;
-  height: 35px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.cart-button:hover {
-  background-color: transparent;
-  border-color: transparent;
-}
-
-.quantity-control div {
-  margin: 0 0.5rem;
-}
-</style>
+  };
+  </script>
+  <style scoped>
+  .picked-for-you {
+    font-family: Arial, sans-serif;
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-bottom: 70px;
+  }
+  
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 10px;
+  }
+  
+  .grid-item {
+    border: 1px solid #202020;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  
+  .item-content {
+    display: flex;
+    height: 100px;
+  }
+  
+  .image-container {
+    width: 30%; /* Changed from 25% to 30% */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+  }
+  
+  .item-image {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+  
+  .item-info {
+    flex-grow: 1;
+    width: 70%; /* Changed from 75% to 70% */
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 15px;
+  }
+  
+  .item-info h3 {
+    font-size: 14px;
+    margin: 0;
+    text-align: left;
+  }
+  
+  .price {
+    font-size: 12px;
+    color: #888;
+    margin: 4px 0;
+    text-align: left;
+  }
+  
+  .quantity-control {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 12px;
+  }
+  
+  .cart-button {
+    background-color: transparent;
+    color: green;
+    border: none;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    font-size: 14px;
+    margin: 0 4px;
+  }
+  
+  .quantity-control span {
+    margin: 0 6px;
+  }
+  </style>
