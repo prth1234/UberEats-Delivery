09ee71400053940d39dd825fd143889cddd9b5e1
prth1234 <107931755+prth1234@users.noreply.github.com>
2024-07-31 16:44:01 +0530

    more changes

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -30,26 +30,20 @@
 
     <div class="content">
       <button class="group-order" @click="openGroupOrderPopup">
-       
-       <svg
-         aria-hidden="true"
-         focusable="false"
-         viewBox="0 0 24 24"
-         class="gj dz gk gl"
-       >
-         <g clip-path="url(#clip0)">
-           <path
-             fill-rule="evenodd"
-             clip-rule="evenodd"
-             d="M10 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zm13 5h-3v-3h-3v3h-3v3h3v3h3v-3h3v-3zM1 23h12.1c-1-1.2-1.6-2.8-1.6-4.5 0-1.7.6-3.3 1.6-4.5H7c-3.3 0-6 2.7-6 6v3z"
-           ></path>
-         </g>
-         <defs>
-           <clipPath id="clip0"><path d="M0 0h24v24H0z"></path></clipPath>
-         </defs>
-       </svg>
-       Group order
-     </button>
+    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" style="width: 24px; height: 24px;">
+      <g clip-path="url(#clip0)">
+        <path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor"
+          d="M10 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zm13 5h-3v-3h-3v3h-3v3h3v3h3v-3h3v-3zM1 23h12.1c-1-1.2-1.6-2.8-1.6-4.5 0-1.7.6-3.3 1.6-4.5H7c-3.3 0-6 2.7-6 6v3z">
+        </path>
+      </g>
+      <defs>
+        <clipPath id="clip0">
+          <path d="M0 0h24v24H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+    Group order
+  </button>
 
      <!-- <div class="searchbarr" style="display: flex; align-items: center; flex-grow: 1; margin-right: 10px; border: none;">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" style="margin-right: 10px;">
@@ -59,7 +53,7 @@
       <input type="text" placeholder="Search in store" style="flex: 1; padding: 10px; border-radius: 4px; font-family: 'Uber Move Light'; font-size: 18px; border: none; background-color: transparent;">
     </div>
  -->
- <div class="toggle-container" style="margin-top: 20px; font-size: 18px;">
+ <div class="toggle-container" style="margin-top: 0px; font-size: 18px;">
       <button class="toggle-button" :class="{ active: activeToggle === 'Delivery' }" @click="setActiveToggle('Delivery')">
         Delivery
       </button>
@@ -1015,19 +1009,22 @@ h1 {
   display: flex;
   align-items: center;
   padding: 8px 16px;
-  width: 120px;
+  width: 156px;
   background-color: #eee;
   border: none;
   border-radius: 20px;
   cursor: pointer;
   color: black;
   font-family: "Uber Move";
+  height: 44px;
 }
 
 .group-order svg {
   margin-right: 8px;
   transform: scale(1);
 }
+
+
 
 
 .description {
@@ -1657,6 +1654,7 @@ input::placeholder {
   font-family: Uber Move;
   height: 44px;
   margin-left: 900px;
+  margin-bottom: -90px;
 }
 
 .toggle-button {
